115c60a24cb613163b676d4a410909c090a94f12
robbie <robbiekruszynski@gmail.com>
2023-11-16 17:37:58 +0300

    add cards v1

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Box, AppBar, Toolbar, Typography, Drawer, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { Box, AppBar, Toolbar, Typography, Drawer, List, ListItem, ListItemIcon, ListItemText, Grid, Card, CardMedia, CardContent } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import InfoIcon from '@mui/icons-material/Info';
+
 
 const drawerWidth = 240;
 
@@ -43,7 +44,35 @@ const Dashboard = () => {
         <Toolbar />
         {/* Content goes here */}
         <Typography paragraph>
-          Welcome to the dashboard!
+        <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}>
+  <Toolbar />
+  <Typography paragraph>
+    Welcome to the dashboard!
+  </Typography>
+
+  <Grid container spacing={3}>
+    {[1, 2, 3, 4, 5, 6].map((tile, index) => (
+      <Grid item xs={12} sm={6} md={4} key={index}>
+        <Card>
+          <CardMedia
+            component="img"
+            height="140"
+            image="/path/to/your/image.jpg" // Replace with your image path
+            alt="Tile Image"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Tile {tile}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Text related to Tile {tile}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    ))}
+  </Grid>
+</Box>
         </Typography>
       </Box>
     </Box>
